65addc3ba4bfed1f300f99a7a5f35f1b259dbb08
mjmoosavizade <mjmoosavizade@gmail.com>
2021-10-14 09:00:29 +0330

    added time zone to node schedule

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.post(`${api}/subscribe`, (req, res) => {
     const payload = JSON.stringify({ title: "یادآوری" });
 
     // Pass object into sendNotification
-    const job = schedule.scheduleJob(`${subscription.minute} ${subscription.hour} * * *`, function () {
+    const job = schedule.scheduleJob(`${subscription.minute} ${subscription.hour} * * *`, 'Asia/Tehran', function () {
         console.log('The answer to life, the universe, and everything!');
         webpush
             .sendNotification(JSON.parse(subscription.subscription), payload)

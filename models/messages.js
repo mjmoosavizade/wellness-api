39748dfbcd374588561521f539312e20888d8531
mjmoosavizade <mjmoosavizade@gmail.com>
2021-11-16 10:28:56 +0330

    edited chat

--- models/messages.js
+++ models/messages.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const messageSchema = mongoose.Schema({
     message: String,
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    to: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     date: { type: Date, default: Date.now, }
 });
 
